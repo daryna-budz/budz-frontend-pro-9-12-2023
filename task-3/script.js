bcc31d72d0663e2eded6eff2908f39b75649b6f9
daryna-budz <darynabudz@gmail.com>
2023-12-11 18:51:42 -0500

    task 3 solution

---
+++ task-3/script.js
@@ -0,0 +1,3 @@
+const firstNum = +prompt("Enter first number :")
+const secondNum = +prompt("Enter second number :")
+alert(`${firstNum} + ${secondNum} = ${firstNum + secondNum} \n${firstNum} - ${secondNum} = ${firstNum - secondNum} \n${firstNum} * ${secondNum} = ${firstNum * secondNum} \n${firstNum} / ${secondNum} = ${firstNum / secondNum}`)
