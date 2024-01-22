4d1dc297786874f01434b6abddb3a7c10c393d41
daryna-budz <darynabudz@gmail.com>
2024-01-22 02:02:32 -0500

    task 10

---
+++ task-10/script.js
@@ -0,0 +1,6 @@
+let num1 = +prompt("Enter first number :");
+let num2 = +prompt("Enter second number :");
+let num3 = +prompt("Enter third number :");
+let mid = (num1+num2+num3)/3;
+
+alert(`Arithmetic mean of ${num1}, ${num2} and ${num3} is ${mid}`)
